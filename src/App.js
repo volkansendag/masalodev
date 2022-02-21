c6b70f68ebdc661d5dfe9da4ff9b77b4f71415d6
Volkan Şendağ <vsendag@gmail.com>
2022-02-21 17:41:32 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Soru buraya yazılacak
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div><input type='checkbox' />Learn React</div>
       </header>
     </div>
   );
