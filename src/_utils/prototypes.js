051575fa39687d6a91a82aa3cb56d6cad2f5e700
Volkan Şendağ <vsendag@gmail.com>
2022-02-23 18:20:54 +0300

    deploy error

--- src/_utils/prototypes.js
+++ src/_utils/prototypes.js
@@ -1,15 +1,18 @@
-Array.prototype.suffle = function () {
-    var array = this;
+Object.defineProperty(Array.prototype, "suffle", {
+    value: function () {
+        "use strict";
+        var array = this;
 
-    if (array && array.length > 0) {
-        let i = array.length - 1;
-        for (; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            const temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+        if (array && array.length > 0) {
+            let i = array.length - 1;
+            for (; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                const temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
         }
-    }
 
-    return array;
-}
+        return array;
+    }
+});
