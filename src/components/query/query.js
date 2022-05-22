c8262dc64ee39f4d00ce091549e86a8e84e45187
masalsendag <100226012+masalsendag@users.noreply.github.com>
2022-05-22 21:36:45 +0300

    Update query.js

--- src/components/query/query.js
+++ src/components/query/query.js
@@ -1,85 +1,94 @@
-import React from 'react';
-import { v4 } from 'uuid';
-import suffle from '../../_utils/functions';
-import './query.css';
-
+import React from "react";
+import { v4 } from "uuid";
+import suffle from "../../_utils/functions";
+import "./query.css";
 
 class Query extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      index: 0,
+      point: 0,
+      value: 0,
+      message: ""
+    };
+  }
 
-        this.state = {
-            index: 0,
-            point: 0,
-            value: 0,
-            message: ""
-        }
+  shouldComponentUpdate() {
+    return true;
+  }
+
+  nextQuery = () => {
+    var state = this.state;
+
+    if (this.props.queries?.length > this.state.index) {
+      state.index++;
     }
 
-    shouldComponentUpdate() {
-        return true;
+    this.setState({ ...state });
+  };
+
+  change = (option) => {
+    var message = this.state.message;
+    var point = this.state.point;
+    var query = this.props.queries[this.state.index];
+
+    if (query.answer === option) {
+      if (query?.point > 0) point += query.point;
+
+      message = "Tebrikler soruyu bildiniz.";
+    } else {
+      message = "Sanırım dersi dinlememişsin.";
     }
 
-    nextQuery = () => {
-        var state = this.state;
+    this.setState({
+      ...this.state,
+      message,
+      point
+    });
 
-        if (this.props.queries?.length > this.state.index) {
-            state.index++;
-        }
+    setTimeout(() => {
+      this.nextQuery();
+      this.setState(() => ({
+        ...this.state,
+        message: ""
+      }));
+    }, 2000);
+  };
 
-        this.setState({ ...state });
-    }
+  render() {
+    var query = this.state.message
+      ? null
+      : this.props.queries[this.state.index];
 
-    change = (evt) => {
-        var data = this.state;
-        var message = this.state.message;
-        var point = this.state.point;
-
-        if (evt.target.value === data.query?.answer) {
-            if (data.query?.point > 0)
-                point += data.query.point;
-
-            message = "Tebrikler soruyu bildiniz."
-        } else {
-            message = "Sanırım dersi dinlememişsin.";
-        }
-
-        this.setState({
-            ...this.state,
-            message,
-            point
-        });
-
-
-        setTimeout(() => {
-            this.nextQuery();
-            this.setState(() => ({
-                ...this.state,
-                message: ""
-            }))
-        }, 2000);
-    }
-
-    render() {
-
-        var query = this.state.message ? null : this.props.queries[this.state.index];
-
-        console.log(query);
-
-        return <div className='query-wrapper'>
-            <p>{this.state.message}</p>
-            {query ? <p className='query'>{query.title}</p> : <></>}
-            <p className='query-options'>
-                {suffle(query?.options).map(option => {
-                    return <label className='query-option' key={v4()}>
-                        <input onChange={this.change} value={option} type="radio" name="soru" /> {option}
-                    </label>
-                })}
-            </p>
-            {this.state.point >= 0 ? <p className='error'>Toplam Puan: {this.state.point}</p> : <></>}
-        </div>
-    }
+    return (
+      <div className="query-wrapper">
+        <p>{this.state.message}</p>
+        {query ? <p className="query">{query.title}</p> : <></>}
+        <p className="query-options">
+          {suffle(query?.options).map((option) => {
+            return (
+              <label className="query-option" key={v4()}>
+                <input
+                  onChange={(e) => this.change(option)}
+                  value={option}
+                  type="radio"
+                  name="soru"
+                />{" "}
+                {option}
+              </label>
+            );
+          })}
+        </p>
+        {this.state.point >= 0 ? (
+          <p className="error">Toplam Puan: {this.state.point}</p>
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Query;
